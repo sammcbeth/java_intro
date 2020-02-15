450cdcddf73e76b203bcbe39e75b84b469ada4ca
sammcbeth <sammcbeth@berkeley.edu>
2020-02-15 14:35:21 -0800

    initial commit

---
+++ byteShortIntLong/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myValue= 10000;
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMax = Integer.MAX_VALUE;
+        System.out.println("integer minimum value: " + myMinIntValue);
+        System.out.println("integer max value: " + myMax);
+        System.out.println("busted" + (myMax + 1));
+        int mynumber = 2147483647;
+    }
+}
---
+++ src/Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello Sam!");
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
+        System.out.println("this is another thing");
+        int mySecondNumber = 12;
+        int myThirdNumber = 13;
+        System.out.println(mySecondNumber);
+        System.out.println(myThirdNumber);
+    }
+}
